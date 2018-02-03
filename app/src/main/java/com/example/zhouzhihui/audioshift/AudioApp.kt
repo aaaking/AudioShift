8a3cb4460e6c76839d825bf1d02b54b8c1e34df8
zhouzhihui <zhouzhihui@htgames.cn>
2018-02-03 19:14:47 +0800

    add leak canary sdk and sign release app

--- app/src/main/java/com/example/zhouzhihui/audioshift/AudioApp.kt
+++ app/src/main/java/com/example/zhouzhihui/audioshift/AudioApp.kt
@@ -3,6 +3,7 @@ package com.example.zhouzhihui.audioshift
 import android.app.Application
 import android.content.Context
 import com.example.zhouzhihui.audioshift.util.ScreenUtil
+import com.squareup.leakcanary.LeakCanary
 
 /**
  * Created by 周智慧 on 02/02/2018.
@@ -16,5 +17,10 @@ class AudioApp : Application() {
         super.onCreate()
         sAppContext = applicationContext
         ScreenUtil.init(sAppContext)
+        if (!LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            LeakCanary.install(this)
+        }
     }
 }
