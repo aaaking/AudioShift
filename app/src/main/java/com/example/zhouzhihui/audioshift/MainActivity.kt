af758980eee2474db36483060caf8b7c0ae6e15f
zhouzhihui <zhouzhihui@htgames.cn>
2018-02-05 17:24:00 +0800

    audio take animation develop

--- app/src/main/java/com/example/zhouzhihui/audioshift/MainActivity.kt
+++ app/src/main/java/com/example/zhouzhihui/audioshift/MainActivity.kt
@@ -15,7 +15,6 @@ import android.view.*
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
-import com.example.zhouzhihui.audioshift.record.Recorder
 import com.example.zhouzhihui.audioshift.ui.BaseActivity
 import com.example.zhouzhihui.audioshift.ui.bigger
 import com.example.zhouzhihui.audioshift.ui.smaller
@@ -25,7 +24,6 @@ import com.example.zhouzhihui.audioshift.util.isCancelled
 import com.zzh.cooldialog.CoolDialog
 import com.zzh.cooldialog.CoolStyle
 import kotlinx.android.synthetic.main.activity_main.*
-import javax.inject.Inject
 
 val PERMISSIONS = arrayOf(Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO, Manifest.permission.MODIFY_AUDIO_SETTINGS)
 val PERMISSIONS_CODE = 1
@@ -138,13 +136,14 @@ class MainActivity : BaseActivity() {
             }
             true
         })
-        audio_take.setOnClickListener {
+        audio_take_container.setOnClickListener {
 //            if (isRecording()) {
 ////                stopRecording()
 //            } else {
 ////                startRecording()
 //            }
             startVoiceStateAnimation(state_animation)
+            startVoiceStateAnimation(audio_take)
         }
     }
 
