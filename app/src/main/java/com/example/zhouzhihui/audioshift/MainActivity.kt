e8f879f37975fb31cf38cd59c6c46af102e8dfd7
zhouzhihui <zhouzhihui@htgames.cn>
2018-02-06 22:24:00 +0800

    remove import of SystemClock

--- app/src/main/java/com/example/zhouzhihui/audioshift/MainActivity.kt
+++ app/src/main/java/com/example/zhouzhihui/audioshift/MainActivity.kt
@@ -9,7 +9,6 @@ import android.graphics.drawable.GradientDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.os.SystemClock
 import android.provider.Settings
 import android.support.v4.app.ActivityCompat
 import android.support.v7.widget.Toolbar
